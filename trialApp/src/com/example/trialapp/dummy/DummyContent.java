4fffc2b4f369aa0eed61b6464fff5ea1b54c6cb1
jayalodha <jayalodha@gmail.com>
2012-08-21 16:34:49 +0530

    Added newline

--- trialApp/src/com/example/trialapp/dummy/DummyContent.java
+++ trialApp/src/com/example/trialapp/dummy/DummyContent.java
@@ -31,6 +31,8 @@ public class DummyContent {
         addItem(new DummyItem("2", "Item 2"));
         addItem(new DummyItem("3", "Item 3"));
  	addItem(new DummyItem("4", "Item 4"));
+
+
     }
 
     private static void addItem(DummyItem item) {
